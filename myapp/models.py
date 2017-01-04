d0b4c648d351204672036a3bf276ffed073ec4be
James Pino <james.c.pino@vanderbilt.edu>
2017-01-04 14:03:25 -0600

    Adding part three  of tutorial

--- myapp/models.py
+++ myapp/models.py
@@ -18,3 +18,21 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Workout(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    num_miles = models.FloatField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
